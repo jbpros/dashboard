592bdf9e9564ef9121b91043adcbd0d91f4f8bfc
Julien Biezemans <jb@jbpros.com>
2010-11-01 16:46:31 +0100

    Ignore CSS files and use Less instead

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.configure('production', function(){
 app.get('/', function(req, res){
   res.render('index.jade', {
     locals: {
-        title: 'Express'
+        title: 'The Dashboard'
     }
   });
 });
