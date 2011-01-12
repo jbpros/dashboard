095511e2a4b946223a4a2e109ddb6af940da2c26
Julien Biezemans <jb@jbpros.com>
2011-01-12 12:36:12 +0100

    Add jasmine-node

---
+++ spec/test_spec.js
@@ -0,0 +1,5 @@
+describe('spec', function() {
+  it('works', function() {
+    expect(true).toBe(true);
+  });
+});
---
+++ specs.js
@@ -0,0 +1,24 @@
+var jasmine = require('jasmine-node');
+var sys = require('sys');
+
+/* what's that for?:
+for(var key in jasmine) {
+  global[key] = jasmine[key];
+}*/
+
+var isVerbose = false;
+var showColors = true;
+process.argv.forEach(function(arg){
+  switch(arg) {
+  case '--color': showColors = true; break;
+  case '--noColor': showColors = false; break;
+  case '--verbose': isVerbose = true; break;
+  }
+});
+
+
+jasmine.executeSpecsInFolder(__dirname + '/spec', function(runner, log){
+  setTimeout(function() {
+    process.exit(runner.results().failedCount);
+  }, 1000);
+}, isVerbose, showColors);
